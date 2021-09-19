69965b3e821358632d9cb6f2d8bab6eb501f54db
ifyour <ifyour@outlook.com>
2021-09-19 23:04:30 +0800

    feat: 前后端联调

--- src/client/.umirc.ts
+++ src/client/.umirc.ts
@@ -4,6 +4,13 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  proxy: {
+    '/api': {
+      // nestjs 开发地址
+      target: 'http://localhost:3000/',
+      changeOrigin: true,
+    }
+  },
   routes: [
     { path: '/', component: '@/pages/index' },
   ],
--- src/client/src/pages/index.tsx
+++ src/client/src/pages/index.tsx
@@ -1,9 +1,27 @@
+import React from 'react';
 import styles from './index.less';
 
 export default function IndexPage() {
+  const [data, setData] = React.useState<string[]>([]);
+
+  const query = React.useCallback(async function () {
+    const res = await window.fetch('/api');
+    const json = await res.json();
+    setData(json);
+  }, [])
+
+  React.useEffect(() => {
+    query();
+  }, [query])
+
   return (
-    <div>
+    <div className={styles.container}>
       <h1 className={styles.title}>Page index</h1>
+      {
+        data.map(value =>
+          <span key={value}>{`${value} `}</span>
+        )
+      }
     </div>
   );
 }
